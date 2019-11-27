5a8ad0ae7b62f554a9fb1eb205bc5a62bdcba106
Waidhoferj <jwaidhof@calpoly.edu>
2019-11-26 23:44:18 -0800

    take out icon sets

--- vue.config.js
+++ vue.config.js
@@ -6,18 +6,6 @@ module.exports = {
     appleMobileWebAppCapable: "yes",
     appleMobileWebAppStatusBarStyle: "default",
     manifestOptions: {
-      icons: [
-        {
-          src: "/icons/favicon-192.png",
-          type: "image/png",
-          sizes: "192x192"
-        },
-        {
-          src: "/icons/favicon-512.png",
-          type: "image/png",
-          sizes: "512x512"
-        }
-      ],
       background_color: "#FFF"
     }
   }
