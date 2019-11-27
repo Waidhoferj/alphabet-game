5e16302023ff320b7fe4c8bc6cee4cf85f08a548
Waidhoferj <jwaidhof@calpoly.edu>
2019-11-26 21:27:13 -0800

    Took out workbox config

--- vue.config.js
+++ vue.config.js
@@ -4,14 +4,6 @@ module.exports = {
     themeColor: "#F0F0F0",
     msTileColor: "#000000",
     appleMobileWebAppCapable: "yes",
-    appleMobileWebAppStatusBarStyle: "default",
-
-    // configure the workbox plugin
-    workboxPluginMode: "InjectManifest",
-    workboxOptions: {
-      // swSrc is required in InjectManifest mode.
-      swSrc: "dev/sw.js"
-      // ...other Workbox options...
-    }
+    appleMobileWebAppStatusBarStyle: "default"
   }
 };
